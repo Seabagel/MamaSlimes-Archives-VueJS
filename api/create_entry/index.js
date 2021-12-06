a97bc23dfe1aca3c8237d975ac4509841efbada7
Seabagel <hsrt1367tim@gmail.com>
2021-12-05 22:51:15 -0800

    Added CosmosDB data binding

--- api/create_entry/index.js
+++ api/create_entry/index.js
@@ -1,39 +1,43 @@
 module.exports = async function (context, req) {
-    context.log("Creating Entry - start");
-  
-    // Try reading the body
-    try {
-      const rb = req.body;
-  
-      if (rb.id && rb.username && rb.messageBody) {
-        context.res = {
-          status: 201, // Success
-          headers: {
-            "content-type": "application/json",
-          },
-          body: {
-            id: rb.id,
-            username: rb.username,
-            messageBody: rb.messageBody,
-          },
-        };
-        context.log("Status: 201 Success");
-      } else {
-        context.res = {
-          status: 400, // Bad Request
-        };
-        context.log("Status: 400 Bad Request");
-      }
-  
-      // Catch Errors
-    } catch (error) {
-      context.res = {
-        status: 500, // Internal Server Error
+  context.log("Creating Entry - start");
+
+  // Try reading the body
+  try {
+    const rb = req.body;
+
+    if (rb.id && rb.username && rb.messageBody) {
+      const data = {
+        id: rb.id,
+        username: rb.username,
+        messageBody: rb.messageBody,
       };
-      context.log("Status: 500 Internal Server Error");
+
+      context.bindings.outputDocument = data;
+
+      context.res = {
+        status: 201, // Success
+        headers: {
+          "content-type": "application/json",
+        },
+        body: data,
+      };
+
+      context.log("Status: 201 Success");
+    } else {
+      context.res = {
+        status: 400, // Bad Request
+      };
+      context.log("Status: 400 Bad Request");
     }
-  
-    context.done();
-    context.log("Creating Entry - finished");
-  };
-  
+
+    // Catch Errors
+  } catch (error) {
+    context.res = {
+      status: 500, // Internal Server Error
+    };
+    context.log("Status: 500 Internal Server Error");
+  }
+
+  context.done();
+  context.log("Creating Entry - finished");
+};
