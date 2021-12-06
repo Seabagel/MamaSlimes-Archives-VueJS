698e4dc023ff079f79051e4aee1110c0223ab31c
Seabagel <hsrt1367tim@gmail.com>
2021-12-05 22:42:26 -0800

    Create Azure function app in /Api folder

--- api/create_entry/index.js
+++ api/create_entry/index.js
@@ -1,13 +1,39 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+    context.log("Creating Entry - start");
+  
+    // Try reading the body
+    try {
+      const rb = req.body;
+  
+      if (rb.id && rb.username && rb.messageBody) {
+        context.res = {
+          status: 201, // Success
+          headers: {
+            "content-type": "application/json",
+          },
+          body: {
+            id: rb.id,
+            username: rb.username,
+            messageBody: rb.messageBody,
+          },
+        };
+        context.log("Status: 201 Success");
+      } else {
+        context.res = {
+          status: 400, // Bad Request
+        };
+        context.log("Status: 400 Bad Request");
+      }
+  
+      // Catch Errors
+    } catch (error) {
+      context.res = {
+        status: 500, // Internal Server Error
+      };
+      context.log("Status: 500 Internal Server Error");
+    }
+  
+    context.done();
+    context.log("Creating Entry - finished");
+  };
+  
