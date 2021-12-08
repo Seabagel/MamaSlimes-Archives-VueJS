0cbf2d4d720d2e53e72fd4c3d93bed3c970eefa0
Seabagel <hsrt1367tim@gmail.com>
2021-12-08 09:10:38 -0800

    Success - Create/Read API
    The website now correctly pulls all the info from the AzureDB, it can create new data, and read data. Now just needs to get DiscordJS to call a GET/POST with Axiom

--- api/read_entries/index.js
+++ api/read_entries/index.js
@@ -1,15 +1,21 @@
-module.exports = async function (context, req) {
+module.exports = function (context) {
   context.log(`Function read_entries has Started`);
-  let documents;
   try {
-    documents = await context.bindings.inputDocument.then((res) =>
-      context.res.json(res)
-    );
+    if (!context.bindings.inputDocument) {
+      context.log("Not found");
+    } else {
+      context.log("Success: " + context.bindings.inputDocument);
+      context.res = {
+        body: context.bindings.inputDocument,
+      };
+    }
   } catch (error) {
     context.res = {
       body: error,
     };
     context.log(error);
   }
+
   context.log(`Function read_entries has Ended`);
+  context.done();
 };
--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
   <ul>
     <li v-for="index in messages" :key="index">
       <HelloWorld :message="index" />
-      <!-- <p>{{ index }}</p> -->
     </li>
   </ul>
 </template>
@@ -24,8 +23,7 @@ export default {
     HelloWorld,
   },
   methods: {
-    async getAPI(
-      variable,
+    async createEntryAPI(
       message_id,
       username,
       msgbody,
@@ -39,28 +37,37 @@ export default {
       )
         .json()
         .then((res) => {
-          variable.push(res);
+          this.messages.push(res);
         });
     },
-  },
-  async mounted() {
-    this.getAPI(
-      this.messages,
-      350989871411625987,
-      "Kamen6885",
-      "POOR STRESS WORK POOR STRESS STOP WORK DRINK DRINK DRINK HANGOVER DRINK HANGOVER DRINK VIOLENT OUTBURST DRINK DEFAULT ON DEBTS DRINK MORTGAGE HOUSE DRINK WIFE LEAVE HIM DRINK SCLEROSIS NO FRIENDS IGNORANT MAKE HIS COUNTRY A SHITHOLE JUST STOP WORKING",
-      "07/03/2021",
-      "https://cdn.discordapp.com/avatars/611794676604993555/3e6186b8c488ae76a04f56ae195df4f7.webp?size=128"
-    );
 
-    this.getAPI(
-      this.messages,
-      350989871411625987,
-      "steamboatwillie1195",
-      "I uh got some business to take care of real quick",
-      "12/01/2021",
-      "https://cdn.discordapp.com/avatars/178326692832477184/af2b67a367bdfe2473b65a04d59b630a.webp?size=128"
-    );
+    async readEntriesAPI() {
+      await (await fetch(`/api/read_entries`)).json().then((res) => {
+        console.log(res);
+        this.messages = res;
+      });
+    },
+  },
+
+  async mounted() {
+    this.readEntriesAPI();
+    // this.createEntryAPI(
+    //   this.messages,
+    //   350989871411625987,
+    //   "Kamen6885",
+    //   "POOR STRESS WORK POOR STRESS STOP WORK DRINK DRINK DRINK HANGOVER DRINK HANGOVER DRINK VIOLENT OUTBURST DRINK DEFAULT ON DEBTS DRINK MORTGAGE HOUSE DRINK WIFE LEAVE HIM DRINK SCLEROSIS NO FRIENDS IGNORANT MAKE HIS COUNTRY A SHITHOLE JUST STOP WORKING",
+    //   "07/03/2021",
+    //   "https://cdn.discordapp.com/avatars/611794676604993555/3e6186b8c488ae76a04f56ae195df4f7.webp?size=128"
+    // );
+
+    // this.createEntryAPI(
+    //   this.messages,
+    //   350989871411625987,
+    //   "steamboatwillie1195",
+    //   "I uh got some business to take care of real quick",
+    //   "12/01/2021",
+    //   "https://cdn.discordapp.com/avatars/178326692832477184/af2b67a367bdfe2473b65a04d59b630a.webp?size=128"
+    // );
   },
 };
 </script>
